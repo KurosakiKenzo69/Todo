4d250bfa7ef8b0fb0d2ec881c444ac22c9137d3a
Kenzo Vongkingkeo <81594327+KurosakiKenzo69@users.noreply.github.com>
2023-02-23 09:54:02 +0100

    responsive + change background button

--- script.js
+++ script.js
@@ -92,3 +92,16 @@ document.addEventListener("keydown", function(event) {
         envoi();
     }
 });
+
+const toggle = document.getElementById('toggle');
+const body = document.body;
+
+toggle.addEventListener('input', e => {
+    const isChecked = e.target.checked;
+
+    if (isChecked) {
+        body.classList.add('dark-theme');
+    } else {
+        body.classList.remove('dark-theme');
+    }
+});
