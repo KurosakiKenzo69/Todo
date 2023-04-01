e178f6fc7725704cd9f1f1d9918c40929e2ac436
KurosakiKenzo69 <81594327+KurosakiKenzo69@users.noreply.github.com>
2023-04-01 23:47:21 +0200

    ajout de la modification des li

--- script.js
+++ script.js
@@ -14,24 +14,7 @@ window.onload = function() {
     var localStorageData = JSON.parse(localStorage.getItem("userData"));
     if (localStorageData) {
         Object.values(localStorageData).forEach(function(value) {
-            var newLi = document.createElement("li");
-            var textNode = document.createTextNode(value);
-            var deleteButton = document.createElement("button");
-            var buttonText = document.createTextNode("X");
-            deleteButton.appendChild(buttonText);
-
-            deleteButton.addEventListener("click", function() {
-                newLi.remove();
-                var localStorageData = JSON.parse(localStorage.getItem("userData"));
-                delete localStorageData[value];
-                localStorage.setItem("userData", JSON.stringify(localStorageData));
-            });
-
-            newLi.appendChild(textNode);
-            newLi.appendChild(deleteButton);
-
-            document.getElementById("ma-liste").appendChild(newLi);
-            deleteButton.classList.add("delete-button");
+            createListItem(value);
         });
     }
 };
@@ -42,47 +25,61 @@ function envoi() {
 
     // Vérifier si l'input a une valeur
     if (inputValue !== '') {
-
-        // Créer un nouvel élément li
-        var newLi = document.createElement("li");
-
-        // Créer un élément texte avec la valeur de l'input
-        var textNode = document.createTextNode(inputValue);
-
-        // Créer un bouton pour supprimer l'élément li
-        var deleteButton = document.createElement("button");
-        var buttonText = document.createTextNode("X");
-        deleteButton.appendChild(buttonText);
-
-        // Ajouter un gestionnaire d'événement pour supprimer l'élément li lorsque le bouton est cliqué
-        deleteButton.addEventListener("click", function() {
-            newLi.remove();
-            var localStorageData = JSON.parse(localStorage.getItem("userData"));
-            delete localStorageData[inputValue];
-            localStorage.setItem("userData", JSON.stringify(localStorageData));
-        });
-
-        // Ajouter le texte et le bouton au nouvel élément li
-        newLi.appendChild(textNode);
-        newLi.appendChild(deleteButton);
-
-        // Ajouter le nouvel élément li à la liste ul
-        document.getElementById("ma-liste").appendChild(newLi);
+        createListItem(inputValue);
 
         // Effacer le champ input
         document.getElementById("inp").value = "";
-
-        // Stocker l'élément li dans le localStorage
-        var localStorageData = JSON.parse(localStorage.getItem("userData")) || {};
-        localStorageData[inputValue] = inputValue;
-        localStorage.setItem("userData", JSON.stringify(localStorageData));
-
-        deleteButton.classList.add("delete-button");
-
     }
     else {
         alert('Veuillez écrire');
     }
+}
+
+function createListItem(value) {
+    // Créer un nouvel élément li
+    var newLi = document.createElement("li");
+
+    // Créer un élément texte avec la valeur de l'input
+    var textNode = document.createTextNode(value);
+
+    // Créer un bouton pour supprimer l'élément li
+    var deleteButton = document.createElement("button");
+    var buttonText = document.createTextNode("X");
+    deleteButton.appendChild(buttonText);
+
+    // Ajouter un gestionnaire d'événement pour supprimer l'élément li lorsque le bouton est cliqué
+    deleteButton.addEventListener("click", function() {
+        newLi.remove();
+        var localStorageData = JSON.parse(localStorage.getItem("userData"));
+        delete localStorageData[value];
+        localStorage.setItem("userData", JSON.stringify(localStorageData));
+    });
+
+    newLi.addEventListener("click", function() {
+        var newValue = prompt("Entrez la nouvelle valeur :");
+        if (newValue !== null && newValue !== "") {
+            var oldValue = newLi.firstChild.textContent;
+            newLi.firstChild.textContent = newValue;
+            var localStorageData = JSON.parse(localStorage.getItem("userData"));
+            delete localStorageData[oldValue];
+            localStorageData[newValue] = newValue;
+            localStorage.setItem("userData", JSON.stringify(localStorageData));
+        }
+    });
+
+    // Ajouter le texte et le bouton au nouvel élément li
+    newLi.appendChild(textNode);
+    newLi.appendChild(deleteButton);
+
+    // Ajouter le nouvel élément li à la liste ul
+    document.getElementById("ma-liste").appendChild(newLi);
+
+    // Stocker l'élément li dans le localStorage
+    var localStorageData = JSON.parse(localStorage.getItem("userData")) || {};
+    localStorageData[value] = value;
+    localStorage.setItem("userData", JSON.stringify(localStorageData));
+
+    deleteButton.classList.add("delete-button");
 }
 
 // Ajouter la fonctionnalité d'envoi avec la touche Entrée
@@ -92,4 +89,3 @@ document.addEventListener("keydown", function(event) {
         envoi();
     }
 });
-
